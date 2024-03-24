ca56a27dddd45ceba239e9bfcfc0fe420b787281
kishyda <marcuspyon@gmail.com>
2024-03-23 22:18:00 -0700

    finished the problem set

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest1 = function (people) {
+    let peopleAge = people.map((person) => {
+        let theirAge = person.yearOfDeath - person.yearOfBirth;
+        return { name: person, age: theirAge }
+    })
+    let peopleSort = peopleAge.slice().sort((a, b) => b.age - a.age);
+    return peopleSort[0]["name"];
 
 };
+
+const findTheOldest = function (people) {
+    let oldestPerson = people.reduce((oldest, person) => {
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if (oldest.age && oldest.age > age) { return oldest }
+        else return { name: person.name, age: age };
+    }, {})
+    return oldestPerson.name;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
